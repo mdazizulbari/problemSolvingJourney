9057c3c23551a6ed1f6482e580e3784cb878d765
smabari <mdazizulbarishafin@gmail.com>
2025-09-19 22:18:43 +0600

    not and shift bitwise operator

---
+++ cpsLessons/1.module_basicProgramming/13_05_shiftOperator.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    /*
+    bitwise shift()
+
+    0000225 / 10
+    0002250
+    0022500
+    0225000 * 10
+    0022500
+    0002250
+    0000225
+    0000022
+    0000002
+    0000000
+    this was in decimals, now lets see in binary
+
+    000011
+    000110 / 2
+    001100 / 2
+    011000 * 2
+    001100 * 2
+    000110 * 2
+    000011 * 2
+    000001 * 2
+    000000
+
+    left shift dose * 2
+    (<< and write how many digits you want to shift)
+
+    right shift dose / 2 
+    (<< and write how many digits you want to shift)
+    */
+    int a;
+    scanf("%d", &a);
+    int b = a << 3;
+    int c = b >> 3;
+
+    printf("b = %d\n", b);
+    printf("c = %d\n", c);
+    
+    return 0;
+}
