bcf1a10e3f6f0b64c08e211029ea2cc232f0cbce
smabari <mdazizulbarishafin@gmail.com>
2025-09-24 18:17:54 +0600

    06_a-election2

---
+++ lessons/cpsAcdemy/1.module_basicProgramming/14_conditionalStatements/06_a-election2.c
@@ -0,0 +1,22 @@
+// https://atcoder.jp/contests/abc366/tasks/abc366_a
+#include <stdio.h>
+
+int main() {
+
+    int n,t,a;
+    scanf("%d%d%d", &n, &t, &a);
+
+    int remaining = n - (t+a);
+    // printf("%d\n",remaining);
+
+    int gap;
+    if(t >= a) gap = t - a;
+    else gap = a - t;
+    // printf("%d\n",gap);
+
+
+    if(remaining < gap) printf("Yes\n");
+    else printf("No\n");
+
+    return 0;
+}
