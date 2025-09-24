2eb630254c876a5f786e0b538c0df458df5284be
smabari <mdazizulbarishafin@gmail.com>
2025-09-24 13:22:40 +0600

    01 conditional statement

---
+++ cpsLessons/1.module_basicProgramming/14_conditionalStatements/01.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    /*
+    input budget, price, available(with 0 or 1)
+    if you have enough budget and the product is available
+    than print "YES"
+    otherwise print "No"
+    */
+
+    int budget, price, available;
+    scanf("%d%d%d", &budget, &price, &available);
+
+    if(price <= budget && available){
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+
+    // int ans ;
+    // printf("%d\n", ans);
+    
+    return 0;
+}
