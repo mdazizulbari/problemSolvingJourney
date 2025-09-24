244d0b8d9bf4cc1891be1b85db48ab38c8b3d0bf
smabari <mdazizulbarishafin@gmail.com>
2025-09-24 17:18:27 +0600

    added another way

--- lessons/cpsAcdemy/1.module_basicProgramming/14_conditionalStatements/04_fanPoll.c
+++ lessons/cpsAcdemy/1.module_basicProgramming/14_conditionalStatements/04_fanPoll.c
@@ -7,6 +7,15 @@ int main() {
     scanf("%d%d%d", &a,&b,&c);
 
     if(a>b && a>c) printf("yes\n");
+    // it will work even if we use the bitwise and (&)
+    /*
+    because,
+    if(a>b & a>c)
+    it becomes,
+    if(1 & 1) // if both are true
+
+    a>b returns ans in bool, true = 1, false = 0
+    */
     else printf("no\n");
     
     return 0;
