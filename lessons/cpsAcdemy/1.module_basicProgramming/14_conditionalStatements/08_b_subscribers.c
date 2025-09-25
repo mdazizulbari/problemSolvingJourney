4d78e5db69eed1f1b47c7f35c70b7df00900896f
smabari <mdazizulbarishafin@gmail.com>
2025-09-26 03:14:24 +0600

    optimized

--- lessons/cpsAcdemy/1.module_basicProgramming/14_conditionalStatements/08_b_subscribers.c
+++ lessons/cpsAcdemy/1.module_basicProgramming/14_conditionalStatements/08_b_subscribers.c
@@ -10,42 +10,41 @@ int main() {
     int ans;
 
     // if(pow(10,3) < a < pow(10,4)){
-    if(1000 < a && a < 10000){
+    if(a < 1000){
+        ans = a;
+    }
+    else if(a < 10000){
         // 1000, 2535
         a = (a/10) * 10;
         ans = a;
         // printf("1\n");
     }
-    else if(pow(10,4) < a && a < pow(10,5)){
+    else if(a < pow(10,5)){
         // 10000, 25356
         a = (a/100) * 100;
         ans = a;
         // printf("10^4\n");
     }
-    else if(pow(10,5) < a && a < pow(10,6)){
+    else if(a < pow(10,6)){
         a = (a/1000) * 1000;
         ans = a;
         // printf("10^5\n");
     }
-    else if(pow(10,6) < a && a < pow(10,7)){
+    else if(a < pow(10,7)){
         a = (a/10000) * 10000;
         ans = a;
         // printf("10^6\n");
     }
-    else if(pow(10,7) < a && a < pow(10,8)){
+    else if(a < pow(10,8)){
         a = (a/100000) * 100000;
         ans = a;
         // printf("10^7\n");
     }
-    else if(pow(10,8) < a && a < pow(10,9)){
+    else if(a < pow(10,9)){
         a = (a/pow(10,6)) * pow(10,6);
         ans = a;
         // printf("10^8\n");
     }
-    else {
-        // printf("2\n");
-        ans = a;
-    } 
 
     printf("%d\n", ans);
     
