abe962414b0e6596ec74b875bf0e7935920b9470
smabari <mdazizulbarishafin@gmail.com>
2025-09-25 13:33:32 +0600

    07_sortingThreeNum

--- lessons/cpsAcdemy/1.module_basicProgramming/14_conditionalStatements/07_sortingThreeNum.c
+++ lessons/cpsAcdemy/1.module_basicProgramming/14_conditionalStatements/07_sortingThreeNum.c
@@ -6,8 +6,40 @@ int main() {
     int a, b, c ;
     scanf("%d%d%d", &a, &b, &c);
 
-    int ans;
-    printf("%d\n", ans);
-    
+    // 2,2,5,7 = called this is non asending or non increasing
+
+    // we have to make the numbers print in asending order
+
+    if(a < b){
+        // a,b
+        if(a > c){
+            printf("%d %d %d\n", c, a, b);
+        }
+        else{
+            // a,b & c,a
+            if(b < c){
+                printf("%d %d %d\n", a,b,c);
+            }
+            else{
+                printf("%d %d %d\n", a,c,b);
+            }
+        }
+    }
+    else{
+        // b,a
+        if(b > c){
+            printf("%d %d %d\n", c, b, a);
+        }
+        else{
+            // b,a & c,a
+            if(a < c){
+                printf("%d %d %d\n", b,a,c);
+            }
+            else{
+                printf("%d %d %d\n", b,c,a);
+            }
+        }
+    }
+
     return 0;
 }
