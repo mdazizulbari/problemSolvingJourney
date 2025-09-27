76a32492b95bf4e29472e50523d5a89213369b67
smabari <mdazizulbarishafin@gmail.com>
2025-09-28 03:12:39 +0600

    0005_devouringDonuts

---
+++ randomProblems/codeChefRandom/0005_devouringDonuts.c
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/DEVDON
+#include <stdio.h>
+
+int main() {
+
+    int x,y;
+    scanf("%d%d", &x, &y);
+
+    int ans = x * y;
+
+    printf("%d\n", ans);
+    
+    return 0;
+}
