0ea947e8368606360c3c8b2e129219b3bac9766b
smabari <mdazizulbarishafin@gmail.com>
2025-09-28 06:47:02 +0600

    0004_a_laylandNumber

---
+++ randomProblems/atCoderRandom/0004_a_laylandNumber.c
@@ -0,0 +1,15 @@
+// https://atcoder.jp/contests/abc320/tasks/abc320_a?lang=en
+#include <stdio.h>
+// #include <math.h>
+
+int main() {
+
+    int a,b;
+    scanf("%d%d", &a, &b);
+
+    // int ans = (pow(a,b) + pow(b,a));
+
+    // printf("%d\n", ans);
+    
+    return 0;
+}
