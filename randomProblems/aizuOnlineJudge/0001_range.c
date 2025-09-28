10fd012495211f72305657620d44905cc6f5fc60
smabari <mdazizulbarishafin@gmail.com>
2025-09-28 07:14:44 +0600

    0001_range

---
+++ randomProblems/aizuOnlineJudge/0001_range.c
@@ -0,0 +1,17 @@
+// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_2_B
+#include <stdio.h>
+
+int main() {
+
+    int a,b,c;
+    scanf("%d%d%d", &a,&b,&c);
+
+    if(a < b && b < c){
+        printf("Yes\n");
+    }
+    else{
+        printf("No\n");
+    }
+    
+    return 0;
+}
