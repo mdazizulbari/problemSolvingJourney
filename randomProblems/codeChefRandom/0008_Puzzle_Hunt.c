c99c0bb367d11fff5eeeef368c3376e713cdb32a
smabari <mdazizulbarishafin@gmail.com>
2025-09-28 08:20:01 +0600

    0008_Puzzle_Hunt

---
+++ randomProblems/codeChefRandom/0008_Puzzle_Hunt.c
@@ -0,0 +1,17 @@
+// https://www.codechef.com/problems/PUZHUNT
+#include <stdio.h>
+
+int main() {
+
+    int a;
+    scanf("%d", &a);
+
+    if(a>5 && a<9){
+        printf("Yes\n");
+    }
+    else{
+        printf("No\n");
+    }
+    
+    return 0;
+}
