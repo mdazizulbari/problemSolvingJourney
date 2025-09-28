e6aa81bfbae5a0edf6665a596a3ed5e16270329b
smabari <mdazizulbarishafin@gmail.com>
2025-09-28 06:52:06 +0600

    randomProblems/codeChefRandom/0006_thalaForAReason.c

---
+++ randomProblems/codeChefRandom/0006_thalaForAReason.c
@@ -0,0 +1,17 @@
+// https://www.codechef.com/problems/THALA7
+#include <stdio.h>
+
+int main() {
+
+    int a;
+    scanf("%d", &a);
+
+    if(a == 7){
+        printf("Thala\n");
+    }
+    else{
+        printf("Sadge\n");
+    }
+    
+    return 0;
+}
