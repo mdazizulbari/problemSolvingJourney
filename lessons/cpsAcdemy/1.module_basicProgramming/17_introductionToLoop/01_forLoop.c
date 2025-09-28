6eac13bfce8e818303709d2a9ea77af4c0c3c922
smabari <mdazizulbarishafin@gmail.com>
2025-09-29 01:52:10 +0600

    01_forLoop

---
+++ lessons/cpsAcdemy/1.module_basicProgramming/17_introductionToLoop/01_forLoop.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+    // for(int i = 1; i <= 7; i+=2){
+    //     printf("%d\n",i);
+    // }
+
+    // for(int i = 5; i >= 1; i--){
+    //     printf("%d\nbrazil\n",i);
+    // }
+
+    // we can do it this way too
+    // int i = 1;
+    // for(; i <= 7; i+=2){
+    //     printf("%d\n",i);
+    // }
+
+    // we can do it this way too
+    int i = 1;
+    for(; i <= 7;){
+        printf("%d\n",i);
+        i+=2;
+    }
+    
+    return 0;
+}
