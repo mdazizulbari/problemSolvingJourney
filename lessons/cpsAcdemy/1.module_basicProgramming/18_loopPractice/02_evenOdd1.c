5837e11a370e13711e4815249bdce3af61ce5830
smabari <mdazizulbarishafin@gmail.com>
2025-09-29 12:41:19 +0600

    update

--- lessons/cpsAcdemy/1.module_basicProgramming/18_loopPractice/02_evenOdd1.c
+++ lessons/cpsAcdemy/1.module_basicProgramming/18_loopPractice/02_evenOdd1.c
@@ -11,6 +11,14 @@ int main() {
         if(num%2 == 0) printf("even\n");
         else printf("odd\n");
     }
+
+    // in a for loop it will look like this,
+    // for(;tc;tc--){
+    //     int num;
+    //     scanf("%d", &num);
+    //     if(num%2 == 0) printf("even\n");
+    //     else printf("odd\n");
+    // }
     
     return 0;
 }
