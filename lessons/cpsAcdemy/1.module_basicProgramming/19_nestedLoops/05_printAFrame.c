675e81d2b6f8787ccb7b8476795c0261f0d038e1
smabari <mdazizulbarishafin@gmail.com>
2025-09-30 14:38:36 +0600

    update

--- lessons/cpsAcdemy/1.module_basicProgramming/19_nestedLoops/05_printAFrame.c
+++ lessons/cpsAcdemy/1.module_basicProgramming/19_nestedLoops/05_printAFrame.c
@@ -11,14 +11,41 @@ int main()
 
         for (int i = 1; i <= h; i++)
         {
-            if (i == 1)
-            {
-                for (int j = 1; j <= w; j++)
-                {
-                    printf("#");
-                }
-            }
-            else if (i == h)
+            // if (i == 1)
+            // {
+            //     for (int j = 1; j <= w; j++)
+            //     {
+            //         printf("#");
+            //     }
+            // }
+            // else if (i == h)
+            // {
+            //     for (int j = 1; j <= w; j++)
+            //     {
+            //         printf("#");
+            //     }
+            // }
+            // else
+            // {
+            //     for (int j = 1; j <= w; j++)
+            //     {
+            //         if (j == 1)
+            //         {
+            //             printf("#");
+            //         }
+            //         else if (j == w)
+            //         {
+            //             printf("#");
+            //         }
+            //         else
+            //         {
+            //             printf(".");
+            //         }
+            //     }
+            // }
+
+            // got to know a better way after seeing the lecture
+            if (i == 1 || i == h)
             {
                 for (int j = 1; j <= w; j++)
                 {
@@ -29,11 +56,7 @@ int main()
             {
                 for (int j = 1; j <= w; j++)
                 {
-                    if (j == 1)
-                    {
-                        printf("#");
-                    }
-                    else if (j == w)
+                    if (j == 1 || j == w)
                     {
                         printf("#");
                     }
