cdad18455fab870cf42a7e03e15bdac3ac37b3ea
smabari <mdazizulbarishafin@gmail.com>
2025-10-01 20:39:19 +0600

    Ageing

---
+++ randomProblems/codeChefRandom/Ageing.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int t, x, y;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        scanf("%d", &x);
+        y = x - 10;
+        printf("%d\n", y);
+    }
+
+    return 0;
+}
