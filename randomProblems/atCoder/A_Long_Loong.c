edda1b8c93094df651da026b092705850ad078a8
smabari <mdazizulbarishafin@gmail.com>
2025-10-02 13:06:05 +0600

    A_Long_Loong

---
+++ randomProblems/atCoder/A_Long_Loong.c
@@ -0,0 +1,17 @@
+// https://atcoder.jp/contests/abc336/tasks/abc336_a
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    printf("L");
+    while (n--)
+    {
+        printf("o");
+    }
+    printf("ng");
+
+    return 0;
+}
