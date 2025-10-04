22345cf9aefaad0ea1202ae91e666e05391d102d
smabari <mdazizulbarishafin@gmail.com>
2025-10-04 14:26:19 +0600

    Fair_Share_Settlement

--- randomProblems/codeChef/Fair_Share_Settlement.c
+++ randomProblems/codeChef/Fair_Share_Settlement.c
@@ -1,1 +1,31 @@
 // https://www.codechef.com/problems/FAIRSHARE
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        int n, k, z;
+        scanf("%d%d", &n, &k);
+        // printf("%d\n", n);
+
+        int fairShare = n / (k + 1);
+        int fSremainder = n % (k + 1);
+
+        // if fSremainder = n, than fairShare = 0;
+        int myTotalPay;
+        if (fSremainder == n)
+        {
+            myTotalPay = fSremainder;
+        }
+        else
+            myTotalPay = fSremainder + fairShare;
+
+        printf("%d\n", myTotalPay);
+    }
+
+    return 0;
+}
--- randomProblems/codeChef/Pizza_Cutting.c
+++ randomProblems/codeChef/Pizza_Cutting.c
@@ -1,1 +1,23 @@
 // https://www.codechef.com/problems/PIZZAC
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        int n;
+        scanf("%d", &n);
+
+        if (n == 1 || n % 2 == 0)
+        {
+            printf("YES\n");
+        }
+        else
+            printf("NO\n");
+    }
+
+    return 0;
+}
