f9cae9114fce9abe4504629de325c6155ab598bc
smabari <mdazizulbarishafin@gmail.com>
2025-10-06 00:12:36 +0600

    Survival_Time

--- randomProblems/codeChef/Survival_Time.c
+++ randomProblems/codeChef/Survival_Time.c
@@ -1,1 +1,20 @@
 // https://www.codechef.com/problems/FIZZBUZZ23_2
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        int n, x, d;
+        scanf("%d %d %d", &n, &x, &d);
+
+        int dailyNeed = 5*x;
+        int canSurvive = n/dailyNeed;
+        int totalSDays = canSurvive+d;
+
+        printf("%d\n", totalSDays);
+    }
+}
