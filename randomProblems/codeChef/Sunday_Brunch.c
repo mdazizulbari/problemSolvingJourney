a23fbba7af9646aeeb4d3f219ca815bfdac98bf1
smabari <mdazizulbarishafin@gmail.com>
2025-10-06 00:00:23 +0600

    Sunday_Brunch

--- randomProblems/codeChef/Sunday_Brunch.c
+++ randomProblems/codeChef/Sunday_Brunch.c
@@ -1,1 +1,19 @@
 // https://www.codechef.com/problems/BRUNCH
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        int ans = x / y;
+
+        if (ans > 20)
+            ans = 20;
+        printf("%d\n", ans);
+    }
+}
