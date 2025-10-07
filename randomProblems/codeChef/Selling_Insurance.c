20e2ee20a9302f163ce9d963a24601845ef8c4c2
smabari <mdazizulbarishafin@gmail.com>
2025-10-07 07:37:54 +0600

    done after seeing the explanation

--- randomProblems/codeChef/Selling_Insurance.c
+++ randomProblems/codeChef/Selling_Insurance.c
@@ -1,1 +1,30 @@
 // https://www.codechef.com/problems/AGENTCHEF
+#include <stdio.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        int x;
+        scanf("%d", &x);
+
+        // 20% = 1/5
+        /*
+        or x/5 = getsInOne
+        and 100/(x/5) = ans
+        or (100*5)/x = ans
+        so ans = 500/x
+        */
+        int ans = 500 / x;
+        int remainder = 500 % x;
+
+        if (remainder)
+            ans++;
+
+        printf("%d\n", ans);
+    }
+
+    return 0;
+}
