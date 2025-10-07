9451f3b903641ebbf4ec7d7752a89dd2342d12f1
smabari <mdazizulbarishafin@gmail.com>
2025-10-07 09:12:27 +0600

    Chess_Pairing

--- randomProblems/codeChef/Chess_Pairing.c
+++ randomProblems/codeChef/Chess_Pairing.c
@@ -1,1 +1,36 @@
 // https://www.codechef.com/problems/CHESS_PAIR
+#include <stdio.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        int x, n;
+        scanf("%d %d", &n, &x);
+        int unRated = (2 * n) - x;
+        int ans;
+
+        if (x > n)
+        {
+            if (x == unRated)
+                ans = 0;
+            else
+            {
+                if (x > unRated)
+                    ans = x - unRated;
+                else if (unRated == 0)
+                    ans = x;
+                else
+                    ans = 0;
+            }
+        }
+        else
+            ans = 0;
+
+        printf("%d\n", ans);
+    }
+
+    return 0;
+}
