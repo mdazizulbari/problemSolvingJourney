b9e64852860b5b52ba94691ae15d3d2b034c921d
smabari <mdazizulbarishafin@gmail.com>
2025-10-11 07:34:59 +0600

    A_Division

---
+++ randomProblems/codeForces/A_Division.c
@@ -0,0 +1,30 @@
+// https://codeforces.com/contest/1669/problem/A
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        int n;
+        int x = 4;
+        scanf("%d", &n);
+        if (1400 <= n && n <= 1599)
+        {
+            x = 3;
+        }
+        else if (1600 <= n && n <= 1899)
+        {
+            x = 2;
+        }
+        else if (1900 <= n)
+        {
+            x = 1;
+        }
+        printf("Division %d\n", x);
+    }
+
+    return 0;
+}
