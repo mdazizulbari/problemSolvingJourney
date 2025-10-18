65b12a9bddd98780b169b2ab649b57b2d5a8a23d
smabari <mdazizulbarishafin@gmail.com>
2025-10-18 12:34:18 +0600

    02

--- lessons/cpsAcdemy/1.module_basicProgramming/23_multiDimentionalArray/02.c
+++ lessons/cpsAcdemy/1.module_basicProgramming/23_multiDimentionalArray/02.c
@@ -2,28 +2,32 @@
 
 int main()
 {
+    // 2 dimentional array
     int classes, maxRoll;
-    int marks[3][4];
+    int marks[4][3];
 
     // getting input
     scanf("%d%d", &classes, &maxRoll);
-    for (int i = 0; i < classes; i++)
+    for (int i = 0; i < classes; i++) // i = classes
     {
-        for (int j = 0; j < maxRoll; j++)
+        for (int j = 0; j < maxRoll; j++) // j = roll
         {
-            scanf("%d", &marks[i][j]);
+            scanf("%d", &marks[j][i]); //j i = roll classes
         }
     }
     for (int i = 0; i < classes; i++)
     {
         for (int j = 0; j < maxRoll; j++)
-            if (marks[i][j] > 50)
+        {
+            // printf("classes = %d ", i);
+            // printf("%d ", marks[j][i]);
+            if (marks[j][i] > 50)
             {
                 printf("g ");
-                // printf("%d ", ara[j][i]);
             }
             else
                 printf("b ");
+        }
         printf("\n");
     }
 
