ef6817a149e70aeaab85f678ac48bfc5ccee92a2
smabari <mdazizulbarishafin@gmail.com>
2025-12-04 10:47:34 +0600

    with for loop

--- lessons/cpsAcdemy/1.module_basicProgramming/25.stringPracticeProblems/02_char_swap.c
+++ lessons/cpsAcdemy/1.module_basicProgramming/25.stringPracticeProblems/02_char_swap.c
@@ -8,17 +8,26 @@ int main()
 
     int n = strlen(str);
 
-    int i = 0;
-    int j = n - 1;
-    while (i < j)
+    // int i = 0;
+    // int j = n - 1;
+    // while (i < j)
+    // {
+    //     printf("i = %d, j = %d\n", i, j);
+    //     char temp = str[i];
+    //     str[i] = str[j];
+    //     str[j] = temp;
+    //     i++;
+    //     j--;
+    // }
+
+    for (int i = 0, j = n - 1; i < j; i++, j--)
     {
         printf("i = %d, j = %d\n", i, j);
         char temp = str[i];
         str[i] = str[j];
         str[j] = temp;
-        i++;
-        j--;
     }
+
     printf("%s\n", str);
 
     return 0;
